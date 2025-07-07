44375b53cc1be927ddf83fa9a626f7b4c047060c
imti <imti@privy.io>
2025-07-07 14:33:54 -0700

    fix: config

--- metro.config.js
+++ metro.config.js
@@ -1,22 +1,49 @@
 // metro.config.js ✅ 适用于 Expo 项目
-
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-// Node.js 模块 polyfill
-config.resolver.extraNodeModules = {
-  ...require('node-libs-react-native'),
-  stream: require.resolve('stream-browserify'),
-  crypto: require.resolve('crypto-browserify'),
-  zlib: require.resolve('browserify-zlib'),
-  util: require.resolve('util/'),
-  buffer: require.resolve('buffer/'),
-  process: require.resolve('process/browser'),
-  assert: require.resolve('assert'),
-  os: require.resolve('os-browserify/browser'),
-  path: require.resolve('path-browserify'),
+const resolveRequestWithPackageExports = (context, moduleName, platform) => {
+  // Package exports in `isows` (a `viem`) dependency are incompatible, so they need to be disabled
+  if (moduleName === "isows") {
+    const ctx = {
+      ...context,
+      unstable_enablePackageExports: false,
+    };
+    return ctx.resolveRequest(ctx, moduleName, platform);
+  }
+  
+  // Package exports in `zustand@4` are incompatible, so they need to be disabled
+  if (moduleName.startsWith("zustand")) {
+    const ctx = {
+      ...context,
+      unstable_enablePackageExports: false,
+    };
+    return ctx.resolveRequest(ctx, moduleName, platform);
+  }
+  
+  // Package exports in `jose` are incompatible, so the browser version version is used
+  if (moduleName === "jose") {
+    const ctx = {
+      ...context,
+      unstable_conditionNames: ["browser"],
+    };
+    return ctx.resolveRequest(ctx, moduleName, platform);
+  }
+  
+  // The following block is only needed if you are
+  // running React Native 0.78 *or older*.
+  if (moduleName.startsWith('@privy-io/')) {
+    const ctx = {
+      ...context,
+      unstable_enablePackageExports: true,
+    };
+    return ctx.resolveRequest(ctx, moduleName, platform);
+  }
+  
+  return context.resolveRequest(context, moduleName, platform);
 };
+
+config.resolver.resolveRequest = resolveRequestWithPackageExports;
 
 module.exports = config;
